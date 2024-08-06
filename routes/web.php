d30acfc29392c9b3f5c5706c1f37d3c8e5623d28
apple <apple@appurunoMacBook-Pro.local>
2024-08-06 14:17:47 +0900

    mainpage

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [App\Http\Controllers\Maincontroller::class, 'index']);
+#Route::get('/', function () {return view('welcome');});
